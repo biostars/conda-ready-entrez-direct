ca494c206e823ffc12822b65154d050479b6edc3
Istvan Albert <istvan.albert@gmail.com>
2021-09-07 21:00:18 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,17 +5,16 @@ osname=`uname -s`
 cputype=`uname -m`
 
 case "$osname-$cputype" in
-  Linux-x86_64 )           platform=Linux ;;
-  Darwin-x86_64 )          platform=Darwin ;;
-  CYGWIN_NT-* | MINGW*-* ) platform=CYGWIN_NT ;;
-  Linux-*arm* )            platform=ARM ;;
-  * )                      platform=UNSUPPORTED ;;
+  Linux-x86_64 )           plt=Linux ;;
+  Darwin-x86_64 )          plt=Darwin ;;
+  Darwin-*arm* )           plt=Silicon ;;
+  Linux-*arm* )            plt=ARM ;;
 esac
 
 # Unpack platform specific binary
-gunzip -c xtract."$platform".gz > xtract
-gunzip -c rchive."$platform".gz > rchive
-gunzip -c transmute."$platform".gz > transmute
+gunzip -c xtract."$platform".gz > xtract."$platform"
+gunzip -c rchive."$platform".gz > rchive."$platform"
+gunzip -c transmute."$platform".gz > transmute."$platform"
 
 # Set executable flag.
-chmod +x xtract rchive transmute
+chmod +x xtract."$platform" rchive."$platform" transmute."$platform"
